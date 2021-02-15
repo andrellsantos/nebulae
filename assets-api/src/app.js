abb03a53d1fb3eabc02661797ff50d6e4273be7b
André Santos <andre.leonhardt.santos@gmail.com>
2021-02-15 11:06:48 -0300

    Adding Swagger documentation

--- assets-api/src/app.js
+++ assets-api/src/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 require('./db/mongoose')
+
+const path = require('path')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load(path.resolve(__dirname,'./api-docs/swagger.yaml'));
+
 const stock = require('./routes/stock');
 
 const app = express()
 const port = process.env.PORT || 3001
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 
 app.use(express.json())
 
