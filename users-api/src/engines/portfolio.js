f36cf59fea591ae7a3f56e11f2b283012db586cc
André Santos <andre.leonhardt.santos@gmail.com>
2021-03-06 11:52:06 -0300

    Improving portfolio calculation

--- users-api/src/engines/portfolio.js
+++ users-api/src/engines/portfolio.js
@@ -14,6 +14,7 @@ exports.calculate = async (user) => {
     }).execPopulate()
     
     var portfolios = new HashMap()
+    // TODO: Create unit tests
     user.transactions.forEach(transaction => {
         const key = transaction.country + "/" + transaction.ticker
         var portfolio = portfolios.get(key)
@@ -28,37 +29,53 @@ exports.calculate = async (user) => {
             portfolios.set(key, portfolio)
         } else {
             portfolio.amount += transaction.amount
-            portfolio.total += (Math.abs(transaction.amount) * transaction.price)
-            portfolio.average = (portfolio.amount == 0) ? 0 : portfolio.total/portfolio.amount
+            if(portfolio.amount == 0) {
+                portfolio.total = 0
+                portfolio.average = 0
+            } else {
+                portfolio.total += Math.abs(transaction.amount) * transaction.price
+                portfolio.average = portfolio.total/portfolio.amount
+            }
             portfolios.set(key, portfolio)
         }
     })
 
     try {
-        const dbPortfolios = await Portfolio.find({
+        // Should remove all portfolios without transactions or when the amount = 0
+        var removePortfolios = await Portfolio.find({
             userId: user._id
         })
-        await Portfolio.remove({
-            userId: user._id
-        })
-        portfolios.values().forEach(portfolio => {
-            var weight
-            const dbPortfolio = dbPortfolios.find(dbPortfolio => {
-                return dbPortfolio.country === portfolio.country && 
-                dbPortfolio.ticker === portfolio.ticker
-            })
-            if(dbPortfolio) {
-                weight = dbPortfolio.weight
+
+        for(const portfolio of portfolios.values()) {
+            var _idRemove = undefined
+            if(portfolio.amount !== 0) {
+                const portfolioDB = await Portfolio.findOneAndUpdate({
+                    userId: user._id,
+                    country: portfolio.country,
+                    ticker: portfolio.ticker
+                }, {
+                    userId: user._id,
+                    country: portfolio.country,
+                    ticker: portfolio.ticker,
+                    amount: portfolio.amount,
+                    average: portfolio.average
+                }, {
+                    upsert: true
+                })
+                portfolioDB ? _idRemove = portfolioDB._id.toString() : undefined
             }
-            new Portfolio({
+            removePortfolios = removePortfolios.filter(remove => {
+                return remove._id.toString() !== _idRemove
+            }) 
+        }
+
+        for(const portfolio of removePortfolios) {
+            await Portfolio.findOneAndDelete({
                 userId: user._id,
                 country: portfolio.country,
-                ticker: portfolio.ticker,
-                weight,
-                amount: portfolio.amount,
-                average: portfolio.average
-            }).save()
-        })
+                ticker: portfolio.ticker
+            })
+        }
     } catch(e) {
         console.log(e)
     }
