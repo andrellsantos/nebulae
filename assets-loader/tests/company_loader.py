b352618d5888989b83c085c75f5bb9c8ab9c9dc6
André Santos <andrellsantos@outlook.com>
2021-06-13 13:32:52 -0300

    Addint unit tests for Python APIs

---
+++ assets-loader/tests/company_loader.py
@@ -0,0 +1,11 @@
+import unittest
+
+class CompanyTest(unittest.TestCase):
+    def test_company(self):
+        self.assertTrue
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
---
+++ assets-loader/tests/quote_loader.py
@@ -0,0 +1,11 @@
+import unittest
+
+class QuoteTest(unittest.TestCase):
+    def test_quote(self):
+        self.assertTrue
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
